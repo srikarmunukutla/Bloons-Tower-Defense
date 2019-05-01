d374917233f65df109adf13aa24a1c30d77850b9
Srikar M <Srikar@Srikars-MacBook-Pro.local>
2019-04-30 23:38:27 -0700

    Adding images

--- src/BTDGameRunner.java
+++ src/BTDGameRunner.java
@@ -10,6 +10,7 @@ import javax.imageio.ImageIO;
 public class BTDGameRunner {
 	private JPanel panel;
 	private JFrame frame = new JFrame("Bloons Tower Defense");
+	private static final String PATH_PREFIX = "images/";
 	
 	private boolean startedGame = false;
 //	private int width = 18, height = 10;
@@ -24,7 +25,7 @@ public class BTDGameRunner {
 			@Override 
 			public void paintComponent(Graphics g) {
 				super.paintComponent(g);
-//				g.drawImage(getImage("BTD Map 1.png"), 0, 0, 700, 520, null);
+				g.drawImage(getImage("bloon1.png"), 20, 20, 20, 20, null);
 //				g.drawImage(getImage("BTD Home Screen.png"), 0, 0, 815, 600, null);
 			}
 		};
@@ -41,5 +42,17 @@ public class BTDGameRunner {
 //		grid = new boolean[10][18];
 		
 	}
+	
+	protected Image getImage(String fn) {
+        Image img = null;
+        fn = PATH_PREFIX+fn;
+        try {
+            img = ImageIO.read(this.getClass().getResource(fn));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return img;
+    }
 
 }
