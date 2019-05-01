1ac4e08de1ececd1521afcbc0ea4ea56377ceb5b
Srikar M <Srikar@Srikars-MacBook-Pro.local>
2019-05-01 13:40:38 -0700

    Revert to original

--- src/Dart.java
+++ src/Dart.java
@@ -1,5 +1,7 @@
+
 public class Dart extends Projectile {
     public Dart(int a, int b){
         super(a,b,"Dart.png");
+        int c = 0;
     }
 }
--- src/Map1.java
+++ src/Map1.java
@@ -1,6 +1,5 @@
 
 public class Map1 extends Map {
-	int a  = 0;
 	public Map1(int r, int c) {
 		super(r, c);
 	}
