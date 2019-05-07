b5bb88e82fbe2b5ead1b4e02fae5bde7f014cff2
Richard Wu <richardwu@Richards-MBP.attlocal.net>
2019-05-06 17:41:19 -0700

    finished ninja and added pics

--- src/NinjaMonkey.java
+++ src/NinjaMonkey.java
@@ -1,4 +1,10 @@
 
-public class NinjaMonkey {
-
+public class NinjaMonkey extends Monkey {
+	public NinjaMonkey(int a, int b) {
+		super(a,b,200,"Ninja_Monkey.png",1,1,50,2);
+	}
+    @Override
+    public Projectile getProj(){
+        return new Shuriken(this.getX(),this.getY());
+    }
 }
