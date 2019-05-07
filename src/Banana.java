2e3138648a86804995b6bbd7982f87c9ef5c1b4c
Richard Wu <richardwu@Richards-MBP.attlocal.net>
2019-05-06 18:38:18 -0700

    edited bananas

--- src/Banana.java
+++ src/Banana.java
@@ -1,7 +1,21 @@
+import java.awt.Image;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Banana {
 	int money;
 	public Banana(int value) {
 		money = value;
 	}
+	
+    protected Image getImage(String fn) {
+        Image img = null;
+        try {
+            img = ImageIO.read(this.getClass().getResource(fn));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return img;
+    }
 }
--- src/BananaFarm.java
+++ src/BananaFarm.java
@@ -1,10 +1,63 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class BananaFarm {
-	int bananaOutput, numBananas, x, y;
+	int bananaOutput, numBananas, x, y, secsbeforereload;
+    private final static String PATH_PREFIX = "images/";
+	Image img = getImage(PATH_PREFIX + "Banana_Farm.png");
+	Rectangle rect;
+	private final static int CONSTANT = 7800;
 	public BananaFarm(int a, int b) {
 		bananaOutput = 20;
 		numBananas = 4;
 		x = a;
 		y = b;
+		secsbeforereload = CONSTANT/numBananas;
 	}
+	
+    protected Image getImage(String fn) {
+        Image img = null;
+        try {
+            img = ImageIO.read(this.getClass().getResource(fn));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return img;
+    }
+    
+    public int getX() {
+    	return x;
+    }
+    
+    public int getY() {
+    	return y;
+    }
+    
+    public int getBananaValue() {
+    	return bananaOutput;
+    }
+    
+    public int getNumBananas() {
+    	return numBananas;
+    }
+    
+    public void draw(Graphics g) {
+    	
+    }
+    
+    public Rectangle getRect() {
+    	return rect;
+    }
+    
+    public void makeBananas() {
+    	if (secsbeforereload > 0) {
+    		secsbeforereload--;
+    		return;
+    	}
+    	Banana b = new Banana(bananaOutput);
+    }
 }
