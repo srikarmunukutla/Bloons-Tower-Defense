4c74ebb88d4565a3ec073f583cee5454d5ad5416
Srikar M <Srikar@Srikars-MacBook-Pro.local>
2019-05-08 17:16:34 -0700

    Srikar - bloons moving

--- src/TesterClass.java
+++ src/TesterClass.java
@@ -3,6 +3,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
@@ -16,8 +17,8 @@ public class TesterClass {
 	private JPanel panel;
 	private JFrame frame = new JFrame("Bloons Tower Defense");
 	private static Pixel[][] grid;
-	private int ticks = 0;
-	private Timer timer;
+	private int ticks = 0, numBloons = 0;
+	private Timer timer, bloonsTimer;
 	private static int height = 676, width = 910;
 
 	public static void main(String[] args) {
@@ -28,8 +29,8 @@ public class TesterClass {
 		BTDMap m1 = new Map1(height, width);
 		m1.initializeTrack();
 		grid = m1.getGrid();
-		m1.addBloon(new Bloon(12,30,93,0, new HashSet<Integer>()));
-		m1.addBloon(new Bloon(10,30,93,0, new HashSet<Integer>()));
+//		m1.addBloon(new Bloon(12,0,93,0, new HashSet<Integer>()));
+//		m1.addBloon(new Bloon(10,0,93,0, new HashSet<Integer>()));
 		panel = new JPanel() {
 			@Override
 			public void paintComponent(Graphics g) {
@@ -61,14 +62,30 @@ public class TesterClass {
 		frame.pack();
 		frame.setVisible(true);
 		
+		bloonsTimer = new Timer(500, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				numBloons++;
+				m1.addBloon(new Bloon(1, 0, 93, 0, new HashSet<Integer>()));
+				if(numBloons == 25) {
+					bloonsTimer.stop();
+				}
+			}
+		});
+		
 		timer = new Timer(1, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				ticks++;
-				for(Bloon b: m1.getBloonList()) {
-					b.update(0.25);
-					if(grid[(int) b.getY()][(int) b.getX()].getAngle() != b.getAngle()) {
-						b.setAngle(grid[(int) b.getY()][(int) b.getX()].getAngle());
+				ArrayList<Bloon> bloons = m1.getBloonList();
+				for(int i = 0; i < bloons.size(); i++) {
+					bloons.get(i).update(0.5);
+					if((int) bloons.get(i).getX() >= grid[0].length || (int) bloons.get(i).getY() >= grid.length) {
+						m1.removeBloon(i);
+						continue;
+					}
+					if(grid[(int) bloons.get(i).getY()][(int) bloons.get(i).getX()].getAngle() != bloons.get(i).getAngle()) {
+						bloons.get(i).setAngle(grid[(int) bloons.get(i).getY()][(int) bloons.get(i).getX()].getAngle());
 					}
 				}
 				panel.repaint();
@@ -81,6 +98,7 @@ public class TesterClass {
 		});
 		
 		timer.start();
+		bloonsTimer.start();
 		
 	}
 
