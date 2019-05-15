4901d90783522254736edb5c7e618b056d37fe88
Richard Wu <richardwu@Richards-MacBook-Pro.local>
2019-05-15 14:03:51 -0700

    ace ace is better than you

--- src/MonkeyAce.java
+++ src/MonkeyAce.java
@@ -1,9 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.swing.JPanel;
 
 public class MonkeyAce extends Monkey {
-	int speed = 12;
+	int radius = 100;
+	double angle = 0, omega = 36, x, y;
 	public MonkeyAce(int a, int b) {
 		super(a,b,100,"Monkey_Ace.png",8,1,200,1);
+		x = a+radius;
+		y = b;
 	}
+	@Override
+    public void update(ArrayList<GameObject> al, double timepassed, JPanel panel, HashMap<Integer,Projectile> gameprojectile){
+		//runs regardless if there are bloons
+		angle += omega * timepassed;
+		setAngle(angle);
+		x = getX()+radius*Math.cos(angle*Math.PI/180);
+		y = getY()-radius*Math.sin(angle*Math.PI/180);
+        if (secsbefreload > 0){
+            secsbefreload--;
+            return;
+        }
+        secsbefreload = getReloadRate();
+        for (int i = 0; i < 8; i++) {
+        	Projectile pr = getProj();
+        	int random = (int) ((Math.random()) * Integer.MAX_VALUE);
+        	gameprojectile.put(random, pr);
+        	int BIGINT = 3780;
+        	double x = this.x+17*Math.cos(i*Math.PI/4+angle);
+            double y = this.y-17*Math.sin(i*Math.PI/4+angle);
+            gameprojectile.get(random).launch((int) x,(int) y, panel,gameprojectile,random, this.getDamage(), al, pierce, r);
+        }
+    }
     @Override
     public Projectile getProj(){
         return new Dart(this.getX(),this.getY());
