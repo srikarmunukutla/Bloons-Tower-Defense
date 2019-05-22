3a23aaace2f18ca40a7fba279ae9dbf237a8be21
Srikar M <Srikar@Srikars-MacBook-Pro.local>
2019-05-22 12:25:04 -0700

    glue gunner

---
+++ src/GlueGunner.java
@@ -0,0 +1,10 @@
+public class GlueGunner extends Monkey {
+
+public GlueGunner(int a,int b) {
+	super(a,b,80,"Glue_Gunner.png",1,0,150,1);
+}
+	@Override
+	public Projectile getProj() {
+        return new GlueProjectile(this.getX(),this.getY());
+    }
+}
