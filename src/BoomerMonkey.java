75f5df75b94eba40d1c32f37dcd047b130f8b6fe
Srikar M <Srikar@Srikars-MacBook-Pro.local>
2019-05-26 23:40:24 -0700

    Srikar ~ Boomer Monkey updates

--- src/BoomerMonkey.java
+++ src/BoomerMonkey.java
@@ -1,9 +1,9 @@
 public class BoomerMonkey extends Monkey {
 	
 	public BoomerMonkey(int a, int b) {
-    	super(a,b,200,"Boomer.png",1,1,300,7,380);
+    	super(a,b,200,"Boomer_Monkey.png",1,1,300,7,380);
         width = 50;
-        height = 50;
+        height = 72;
         hasrange = true;
         setImgRect();
     }
--- src/Boomerang.java
+++ src/Boomerang.java
@@ -1,6 +1,6 @@
 public class Boomerang extends Projectile {
 	
 	public Boomerang(int a,int b) {
-		super(a,b,"Boomerang.png",40,18,true);
+		super(a,b,"Boomerang.png",35,35,true);
 	}
 }
