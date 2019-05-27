762afbb18edbc3555532da93e6ad0530fe4f2cd3
Richard Wu <richardwu@Richards-MBP.attlocal.net>
2019-05-26 19:22:29 -0700

    AYY PEE EYE

--- src/API.java
+++ src/API.java
@@ -1,7 +1,1 @@
-
-public class API extends Projectile {
-	
-    public API(int a, int b) {
-        super(a,b,"API.png", 20, 26, false);
-    }
-}
+public class API extends Projectile {public API(int a, int b) {super(a,b,"API.png", 20, 26, false);}}
