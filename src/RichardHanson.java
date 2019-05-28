84bbe5436cd1de1ece984fa2c3afc1d93cb13e1f
Srikar M <Srikar@Srikars-MacBook-Pro.local>
2019-05-28 08:53:51 -0700

    Srikar ~ Fixed Hanson

--- src/RichardHanson.java
+++ src/RichardHanson.java
@@ -1,1 +1,14 @@
-public class RichardHanson extends Monkey {public RichardHanson(int a, int b) {super(a,b,200,"Hanson.png",1,1,100,32,1337);width = 50;height = 62;hasrange = true;setImgRect();}@Override public Projectile getProj(){return new API(this.getX(),this.getY());}}
+public class RichardHanson extends Monkey {
+	
+	public RichardHanson(int a, int b) {
+		super(a,b,200,"Hanson.png",1,1,100,32,1337);
+		width = 50;
+		height = 62;
+		hasrange = true;
+		setImgRect();
+	}
+	
+	@Override public Projectile getProj() {
+		return new API(this.getX(),this.getY());
+	}
+}
