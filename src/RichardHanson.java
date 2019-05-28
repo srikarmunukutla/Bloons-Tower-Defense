1998fb1ba78683138ccebbc86b441f678b8f1bd5
Richard Wu <richardwu@Richards-MBP.attlocal.net>
2019-05-27 17:20:04 -0700

    hanson

--- src/RichardHanson.java
+++ src/RichardHanson.java
@@ -1,16 +1,1 @@
-
-public class RichardHanson extends Monkey {
-	public RichardHanson(int a, int b) {
-		super(a,b,200,"Hanson.png",1,1,100,32,1337);
-		width = 50;
-		height = 62;
-		hasrange = true;
-		setImgRect();
-	}
-	
-    @Override
-    public Projectile getProj(){
-        return new API(this.getX(),this.getY());
-    }
-
-}
+public class RichardHanson extends Monkey {public RichardHanson(int a, int b) {super(a,b,200,"Hanson.png",1,1,100,32,1337);width = 50;height = 62;hasrange = true;setImgRect();}@Override public Projectile getProj(){return new API(this.getX(),this.getY());}}
